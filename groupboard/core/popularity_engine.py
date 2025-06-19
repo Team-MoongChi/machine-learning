5a62636ea50649e108e52a16ebb7878bbf26774a
suri-pu-bi <ymg12347@gmail.com>
2025-06-19 10:57:11 +0900

    feat: Add activity statistics calculation and merge with group board info

--- groupboard/core/popularity_engine.py
+++ groupboard/core/popularity_engine.py
@@ -20,5 +20,27 @@ class PopularityEngine:
         regional_group_ids = regional_groups['id'].tolist()
         return recent_activities_df[recent_activities_df['group_board_id'].isin(regional_group_ids)]
 
-    
+    def calculate_activity_stats(
+        self, regional_activities: pd.DataFrame
+    ) -> pd.DataFrame:
+        """공구방별 활동 통계(찜 수, 최근/최초 활동일) 계산"""
+        activity_stats = regional_activities.groupby('group_board_id').agg({
+            'user_id': 'count',
+            'created_at': ['max', 'min']
+        }).reset_index()
+        activity_stats.columns = [
+            'group_board_id', 'recent_favorites', 'latest_favorite', 'earliest_favorite'
+        ]
+        return activity_stats
+
+    def merge_group_activity(
+        self, regional_groups: pd.DataFrame, activity_stats: pd.DataFrame
+    ) -> pd.DataFrame:
+        """공구방 정보와 활동 통계 병합"""
+        return regional_groups.merge(
+            activity_stats,
+            left_on='id',
+            right_on='group_board_id',
+            how='left'
+        )
     
