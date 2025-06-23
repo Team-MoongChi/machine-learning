d000a6f1104f5c8806e735d0471b6f77f50e6010
suri-pu-bi <ymg12347@gmail.com>
2025-06-23 16:33:03 +0900

    feat: Add newuser router

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -1,4 +1,5 @@
 from api.routers.group_board import router as group_board
 from api.routers.recommendation import router as recommendation
+from api.routers.new_user import router as new_user
 
-__all__ = ['group_board', 'recommendation']
+__all__ = ['group_board', 'recommendation', 'new_user']
