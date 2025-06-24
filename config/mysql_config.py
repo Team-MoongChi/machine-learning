f8bc4bcef477a25e36a15373cc55074834b6e795
suri-pu-bi <ymg12347@gmail.com>
2025-06-25 01:58:40 +0900

    fix: Rename mysql password

--- config/mysql_config.py
+++ config/mysql_config.py
@@ -4,7 +4,7 @@ MYSQL_URL = Secret.load("mysql-url",  _sync=True)
 MYSQL_DATABASE = Secret.load("mysql-database",  _sync=True)
 MYSQL_PORT = Secret.load("mysql-port",  _sync=True)
 MYSQL_USERNAME = Secret.load("mysql-username",  _sync=True)
-MYSQL_PASSWORD = Secret.load("mysql-passowrd",  _sync=True)
+MYSQL_PASSWORD = Secret.load("mysql-password",  _sync=True)
 
 MYSQL_CONFIG = {
     "url": MYSQL_URL.get(),
