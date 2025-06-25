b8ce49c2a6b6f867910cb006b838a72afefdb51e
suri-pu-bi <ymg12347@gmail.com>
2025-06-26 00:04:45 +0900

    refactor: Change config again

--- config/mysql_config.py
+++ config/mysql_config.py
@@ -1,14 +1,17 @@
-import os
-from dotenv import load_dotenv
+from prefect.blocks.system import Secret
 
-load_dotenv()
+MYSQL_URL = Secret.load("mysql-url",  _sync=True)
+MYSQL_DATABASE = Secret.load("mysql-database",  _sync=True)
+MYSQL_PORT = Secret.load("mysql-port",  _sync=True)
+MYSQL_USERNAME = Secret.load("mysql-username",  _sync=True)
+MYSQL_PASSWORD = Secret.load("mysql-password",  _sync=True)
 
 MYSQL_CONFIG = {
-    "url": os.getenv("MYSQL_URL"),
-    "database": os.getenv("MYSQL_DATABASE"),
-    "port": int(os.getenv("MYSQL_PORT", 3306)),
-    "username": os.getenv("MYSQL_USERNAME"),
-    "password": os.getenv("MYSQL_PASSWORD"),
+    "url": MYSQL_URL.get(),
+    "database": MYSQL_DATABASE.get(),
+    "port": MYSQL_PORT.get(),
+    "username": MYSQL_USERNAME.get(),
+    "password": MYSQL_PASSWORD.get(),
     "charset": "utf8mb4",
     "timezone": "Asia/Seoul",
 }
